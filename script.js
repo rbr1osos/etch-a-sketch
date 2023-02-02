a8f81b839f694b36355c4ec6a8abfdd53ec2792d
Ryan Briosos <rybri415@yahoo.com>
2023-02-01 17:13:16 -0800

    hover over squares change to black

--- script.js
+++ script.js
@@ -1,45 +1,61 @@
-//********NEED TO DO */
-/*
-- Create square color change when hovered
-- create reset button that changes it back to 16x16
-- change number input into a number SLIDER ******
-- CSS things *******
-
-*/
-//const number = quantity;
+//********add a clear button***/
 
 const grid = document.getElementById("grid_div");
 const userInput = document.getElementById("quantity");
-const reset_button = document.querySelector(".reset");
+const clear_button = document.querySelector(".clear");
 const square_div = document.querySelector(".square");
-const output_div = document.querySelector('.grid-output')
+const output_div = document.querySelector('.grid-output');
+const square = document.querySelector("div");
 
-//popular choices
-const div_4 = document.getElementById("choice-4");
-const div_16 = document.getElementById("choice-16");
-const div_24 = document.getElementById("choice-24");
-const div_32 = document.getElementById("choice-32");
-const div_48 = document.getElementById("choice-48");
-const div_64 = document.getElementById("choice-64");
-const div_80 = document.getElementById("choice-80");
-const div_88 = document.getElementById("choice-88");
-const div_100 = document.getElementById("choice-100");
+const black_button = document.querySelector(".black");
+const gray_button = document.querySelector(".gray");
+const rainbow_button = document.querySelector(".rainbow");
+const erase = document.querySelector(".eraser");
 
+
+//creates default grid
 function createGrid(){
-  grid.innerHTML='';
-  grid.style.setProperty(
-    'grid-template-columns',
-    'repeat(16,2fr)'
-  );
-  grid.style.setProperty(
-    'grid-template-rows',
-    'repeat(16, 2fr)'
-  );
   for (let n=0; n < 256; n++){
     const div = document.createElement('div');
     div.classList.add('square') //create class .square
     grid.appendChild(div);
+    output_div.innerText='';
+    output_div.innerText= 16;
   };
+}
+
+function change_black(){
+  grid.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor='black';
+  });
+}
+
+function change_gray(){
+  grid.addEventListener("mouseover", function(event) {
+    event.target.style.backgroundColor='gray';
+  });
+}
+
+
+//clear
+function eraseColor(){
+  let gridPixels = grid.querySelectorAll('div');
+  gridPixels.forEach(gridPixels=> gridPixels.style.backgroundColor='ffffff');
+};
+
+
+//Selects Square and  replaces color
+function colorChange(color){
+  console.log(color);
+  switch (color){
+    case 'gray':
+      change_gray();
+      break;
+    case undefined:
+      change_black();
+      break;
+
+  }
 }
 
 //upgrade grid
@@ -73,14 +89,22 @@ function upgradeGrid(num){
 
 //Calls function
   createGrid();
+
 //DOM EVENTS
-  reset_button.addEventListener('click',createGrid);
+  clear_button.addEventListener('click',eraseColor)
+
   userInput.addEventListener("mousemove", () => { upgradeGrid(`${userInput.value}`)}); 
   userInput.addEventListener("mousemove", () => { changeNumber(`${userInput.value}`)}); 
+  
+  //default color
+  grid.addEventListener("mouseover", function(event) {
+    colorChange();
+  });
+  
 
-
-
-
+  //gray
+  gray_button.addEventListener('click',()=>{
+    colorChange('gray')});
 
 
 /*
@@ -94,14 +118,21 @@ div_64.addEventListener("click", () => { upgradeGrid(64)});
 div_80.addEventListener("click", () => { upgradeGrid(80)}); 
 div_88.addEventListener("click", () => { upgradeGrid(88)}); 
 div_100.addEventListener("click", () => { upgradeGrid(100)}); 
-
-
-
-
 */
 
 
-
+/*
+//popular choices
+const div_4 = document.getElementById("choice-4");
+const div_16 = document.getElementById("choice-16");
+const div_24 = document.getElementById("choice-24");
+const div_32 = document.getElementById("choice-32");
+const div_48 = document.getElementById("choice-48");
+const div_64 = document.getElementById("choice-64");
+const div_80 = document.getElementById("choice-80");
+const div_88 = document.getElementById("choice-88");
+const div_100 = document.getElementById("choice-100");
+*/
 
 
 
